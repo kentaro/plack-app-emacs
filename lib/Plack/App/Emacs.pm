e3bedd355b8070f1bb8919771a509d0397ad7e64
Kentaro Kuribayashi <kentarok@gmail.com>
2012-01-28 00:51:41 +0900

    initial import

---
+++ lib/Plack/App/Emacs.pm
@@ -0,0 +1,39 @@
+package Plack::App::Emacs;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Plack::App::Emacs - blah blah blah
+
+=head1 SYNOPSIS
+
+  use Plack::App::Emacs;
+
+=head1 DESCRIPTION
+
+Plack::App::Emacs is
+
+=head1 AUTHOR
+
+Kentaro Kuribayashi E<lt>kentarok@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Kentaro Kuribayashi
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
